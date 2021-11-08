ba033e1fed88bfd3126d13f12ae989a6da77c67d
Joe <joelin890120@gmail.com>
2021-11-09 02:03:29 +0800

    feat: webapi getnews

--- src/WebAPI.js
+++ src/WebAPI.js
@@ -1,1 +1,14 @@
 import axios from 'axios'
+const backend_SERVERURL = process.env.Heroku_backend
+
+export const getNews = async () => {
+    try {
+        let res = await axios.get(backend_SERVERURL + `/newslist`, {
+            headers: { "content-type": "application/json" }
+        })
+        return res
+    } catch (error) {
+        return handle_Error(error);
+    }
+};
+
