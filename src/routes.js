0b8619bab19c3a9435f4a3aeb96097760cbe6c5e
Joe_mac <joelin890120@gmail.com>
2021-11-11 19:05:15 +0800

    feat: add trend, tutorial pages

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
 const NewsPage = React.lazy(() => import('./views/pages/NewsPage'))
+const TrendPage = React.lazy(() => import('./views/pages/TrendPage'))
+const TutorialPage = React.lazy(() => import('./views/pages/TutorialPage'))
 
 const routes = [
   { path: '/', exact: true, name: 'NewsPage', component: NewsPage },
+  { path: '/trend', exact: true, name: 'TrendPage', component: TrendPage },
+  { path: '/tutorial', exact: true, name: 'TutorialPage', component: TutorialPage },
 ]
 
 export default routes
---
+++ src/views/pages/TutorialPage.js
@@ -0,0 +1,5 @@
+import React from 'react'
+const TutorialPage =()=> {
+    return ("TutorialPage")
+}
+export default TutorialPage
