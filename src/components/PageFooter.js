84b10409745b6f37733254cf69168510c7c05c26
Joe_mac <joelin890120@gmail.com>
2021-11-11 14:33:25 +0800

    feat: add temp footer

--- src/components/PageFooter.js
+++ src/components/PageFooter.js
@@ -2,15 +2,177 @@ import React from 'react'
 
 const PageFooter = () => {
     return (
-        <footer className="page-footer">
-        <div className="footer-copyright">
-            <div className="container">
-            © 2019 Copyright
-            <a className="grey-text text-lighten-4 right" href="#!">
-                More Links
-            </a>
+
+        <footer class="page-footer bg-grey-dark-key">
+            <div class="container">
+                <div class="footer-top">
+                    <div class="row">
+                        <div class="col-lg-2 col-md-4 col-6">
+                            <div class="footer-widget">
+                                <h2 class="footer-widget-title">
+                                    World
+                                </h2>
+
+                                <ul class="footer-nav">
+                                    <li><a href="#">U.N.</a></li>
+                                    <li><a href="#">Conflicts</a></li>
+                                    <li><a href="#">Terrorism</a></li>
+                                    <li><a href="#">Disasters</a></li>
+                                    <li><a href="#">Global Economy</a></li>
+                                    <li><a href="#">Global Economy</a></li>
+                                    <li><a href="#">Environment</a></li>
+                                    <li><a href="#">Religion</a></li>
+                                    <li><a href="#">Scandals</a></li>
+                                </ul>
+
+                            </div>
+
+                        </div>
+
+
+                        <div class="col-lg-2 col-md-4 col-6">
+                            <div class="footer-widget">
+                                <h2 class="footer-widget-title">
+                                    Politics
+                                </h2>
+
+                                <ul class="footer-nav">
+                                    <li><a href="#">Executive</a></li>
+                                    <li><a href="#">Senate</a></li>
+                                    <li><a href="#">House</a></li>
+                                    <li><a href="#">Judiciary</a></li>
+                                    <li><a href="#">Foreign policy</a></li>
+                                    <li><a href="#">Polls</a></li>
+                                    <li><a href="#">Elections</a></li>
+                                </ul>
+
+                            </div>
+
+                        </div>
+
+
+                        <div class="col-lg-2 col-md-4 col-6">
+                            <div class="footer-widget">
+                                <h2 class="footer-widget-title">
+                                    Entertainment
+                                </h2>
+
+                                <ul class="footer-nav">
+                                    <li><a href="#">Celebrity News</a></li>
+                                    <li><a href="#">Movies</a></li>
+                                    <li><a href="#">TV News</a></li>
+                                    <li><a href="#">Music News</a></li>
+                                    <li><a href="#">Style News</a></li>
+                                    <li><a href="#">Entertainment Video</a></li>
+                                </ul>
+
+                            </div>
+
+                        </div>
+
+
+                        <div class="col-lg-2 col-md-4 col-6">
+                            <div class="footer-widget">
+                                <h2 class="footer-widget-title">
+                                    Business
+                                </h2>
+
+                                <ul class="footer-nav">
+                                    <li><a href="#">Markets</a></li>
+                                    <li><a href="#">Politics</a></li>
+                                    <li><a href="#">Technology</a></li>
+                                    <li><a href="#">Features</a></li>
+                                    <li><a href="#">Business Leaders</a></li>
+                                </ul>
+
+                            </div>
+
+                        </div>
+
+
+                        <div class="col-lg-2 col-md-4 col-6">
+                            <div class="footer-widget">
+                                <h2 class="footer-widget-title">
+                                    Health
+                                </h2>
+
+                                <ul class="footer-nav">
+                                    <li><a href="#">Healthy Living</a></li>
+                                    <li><a href="#">Medical Research</a></li>
+                                    <li><a href="#">Mental Health</a></li>
+                                    <li><a href="#">Cancer</a></li>
+                                    <li><a href="#">Heart Health</a></li>
+                                    <li><a href="#">Children's Health</a></li>
+                                </ul>
+
+                            </div>
+
+                        </div>
+
+
+                        <div class="col-lg-2 col-md-4 col-6">
+                            <div class="footer-widget">
+                                <h2 class="footer-widget-title">
+                                    About
+                                </h2>
+
+                                <ul class="footer-nav">
+                                    <li><a href="#">Contact Us</a></li>
+                                    <li><a href="#">Careers</a></li>
+                                    <li><a href="#">Fox Around the World</a></li>
+                                    <li><a href="#">Advertise With Us</a></li>
+                                    <li><a href="#">Ad Choices</a></li>
+                                    <li><a href="#">Media Relations</a></li>
+                                    <li><a href="#">Compliance</a></li>
+                                </ul>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+
+                <div class="footer-mid">
+                    <div class="row align-items-center">
+                        <div class="col-md">
+                            <div class="footer-logo-container">
+                                <a href="index.html">
+                                    <img src="assets/images/logo-symbol.svg" alt="footer logo" class="footer-logo" />
+                                </a>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+
+                <div class="footer-bottom">
+                    <ul class="footer-bottom-links">
+                        <li><a href="#">Terms of Use</a></li>
+                        <li><a href="#">Accessibility & CC</a></li>
+                        <li><a href="#">AdChoices</a></li>
+                        <li><a href="#">Modern Slavery Act Statement</a></li>
+                        <li><a href="#">Advertise with us</a></li>
+                        <li><a href="#">Papr Store</a></li>
+                        <li><a href="#">Newsletters</a></li>
+                        <li><a href="#">Transcripts</a></li>
+                        <li><a href="#">License Footage</a></li>
+                        <li><a href="#">Sitemap</a></li>
+                    </ul>
+
+
+                    <p class="axil-copyright-txt">
+                        Copyright © 2021 BCD Teams.
+                    </p>
+                </div>
+
             </div>
-        </div>
+
         </footer>
     );
 }
