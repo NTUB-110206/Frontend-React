7ce8b6ea0023810ac02e728cd6f1a4d37b19d09a
Joe_mac <joelin890120@gmail.com>
2021-12-09 18:12:15 +0800

    feat: newsPage data from local storge

--- src/views/pages/NewsPage.js
+++ src/views/pages/NewsPage.js
@@ -2,18 +2,16 @@ import React, { useState, useEffect } from 'react'
 import LargeNewsLayout from '../components/LargeNewsLayout'
 import MediumNewsLayout from '../components/MediumNewsLayout'
 import SmallNewsLayout from '../components/SmallNewsLayout'
-const WEBAPI = require('../../WebAPI');
+const utils = require('../../utils');
 
 const NewsPage = () => {
     const [top_news, setTop_news] = useState([]);
     const [hot_news, setHot_news] = useState([]);
     useEffect(() => {
         async function fetch() {
-            let newslist = await WEBAPI.getNews()
-            if (newslist?.status != 200 || newslist?.data == "") {
-            } else {
-                newslist = newslist['data']['data']['news']
-                console.log(newslist)
+            let newslist = await utils.getNews()
+            if(newslist){
+                newslist = JSON.parse(newslist)
                 setTop_news(newslist.slice(0, 5))
                 setHot_news(newslist.slice(5))
             }
