090a8086ffc8fcff0c719b48f51ae059a124c137
Joe_mac <joelin890120@gmail.com>
2021-12-09 18:11:39 +0800

    feat: save news to local storge

--- src/layout/DefaultLayout.js
+++ src/layout/DefaultLayout.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { SideNav, PageHeader, PageContent, PageFooter } from '../components/index'
-
+const WEBAPI = require('../WebAPI');
+const utils = require('../utils');
 
 const DefaultLayout = () => {
+  useEffect(() => {
+      async function fetch() {
+          let newslist = await WEBAPI.getNews()
+          if (newslist?.status != 200 || newslist?.data == "") {
+          } else {
+            newslist = newslist['data']['data']['news']
+            utils.setNews(JSON.stringify(newslist))
+          }
+      }
+      fetch()
+  }, []);
+  
   return (
     <div className="main-content">
       <SideNav />
