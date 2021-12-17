e4a900272654080e69d7e0128ba33b03212bdc98
Joe_mac <joelin890120@gmail.com>
2021-12-17 13:06:33 +0800

    feat: TutorialPage img width:100%

--- src/views/pages/TutorialPage.js
+++ src/views/pages/TutorialPage.js
@@ -13,22 +13,22 @@ const TutorialPage = () => {
                                     </div>
                                     <h2>買比特幣第一步：註冊幣安帳號並通過實名制認證</h2>
                                     <h3>1. 首先進入幣安網頁並註冊帳號</h3>
-                                    <img src='https://i.imgur.com/5m1BPpM.png' />
+                                    <img src='https://i.imgur.com/5m1BPpM.png' style={{width:"100%"}}/>
                                     <h3>2. 到信箱收信，輸入驗證碼，就會看到下圖畫面，點「前往用戶中心」</h3>
-                                    <img src='https://imgur.com/5bumzrD.png' />
+                                    <img src='https://imgur.com/5bumzrD.png' style={{width:"100%"}}/>
                                     <h3>3. 接著要設定兩階段驗證，這樣也可以增加安全性，駭客就算擁有你的帳號密碼也無法盜取你的加密貨幣；設定過程很簡單，可以下載 Google 驗證器或者用手機驗證碼</h3>
-                                    <img src='https://imgur.com/F2n7VyN.png' />
+                                    <img src='https://imgur.com/F2n7VyN.png' style={{width:"100%"}}/>
                                     <h3>4. 完成安全性驗證後，接著在用戶中心點擊「去認證」完成 KYC 身份驗證</h3>
-                                    <img src='https://imgur.com/CjFMrfF.png' />
+                                    <img src='https://imgur.com/CjFMrfF.png' style={{width:"100%"}}/>
                                     <h3>5. 這裡點「立即開始」，幣安會要求你輸入姓名、地址等等，並完成身分驗證</h3>
-                                    <img src='https://imgur.com/GnD4BCa.png' />
+                                    <img src='https://imgur.com/GnD4BCa.png' style={{width:"100%"}}/>
                                     <h2>買比特幣第二步：入金至幣安帳號</h2>
                                     <p>要在幣安買比特幣，首先推薦用「銀行電匯」的方式入金，也就是匯一筆錢到幣安帳號，然後就可以在幣安用這筆錢買比特幣等加密貨幣。</p>
                                     <h3>1. 在上面的選單選「買幣」「銀行充值」</h3>
-                                    <img src='https://imgur.com/mkwqd4I.png' />
+                                    <img src='https://imgur.com/mkwqd4I.png' style={{width:"100%"}}/>
                                     <p>這裡只能用選擇美金，不能用新台幣；但你就算沒有銀行的外幣帳號也沒關係，在銀行櫃台就可以把台幣直接換成美金並匯款。</p>
                                     <h3>2. 選擇「繼續」</h3>
-                                    <img src='https://imgur.com/VpGvIVX.png' />
+                                    <img src='https://imgur.com/VpGvIVX.png' style={{width:"100%"}}/>
                                 </div>
                             </div>
                         </div>
